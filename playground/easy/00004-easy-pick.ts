2c4f5342f809b00a0770b1b3d1534fe279a0995e
yusunghyun <ysh1513@naver.com>
2023-08-13 23:00:52 +0900

    ✅ 4번 문제

--- playground/easy/00004-easy-pick.ts
+++ playground/easy/00004-easy-pick.ts
@@ -29,8 +29,9 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type MyPick<T, K> = any
-
+type MyPick<T, K extends keyof T> = {
+  [key in K]: T[key]
+}
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
