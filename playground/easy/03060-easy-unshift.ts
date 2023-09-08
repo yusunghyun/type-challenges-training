c321ccd7cfdda86affa97774d5b7832e7f5dd246
yusunghyun <ysh1513@naver.com>
2023-09-08 23:00:49 +0900

    ✅ 3060번 문제

--- playground/easy/03060-easy-unshift.ts
+++ playground/easy/03060-easy-unshift.ts
@@ -18,7 +18,7 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type Unshift<T, U> = any;
+type Unshift<T extends unknown[], U> = [U, ...T];
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from "@type-challenges/utils";
@@ -26,7 +26,7 @@ import type { Equal, Expect } from "@type-challenges/utils";
 type cases = [
   Expect<Equal<Unshift<[], 1>, [1]>>,
   Expect<Equal<Unshift<[1, 2], 0>, [0, 1, 2]>>,
-  Expect<Equal<Unshift<["1", 2, "3"], boolean>, [boolean, "1", 2, "3"]>>,
+  Expect<Equal<Unshift<["1", 2, "3"], boolean>, [boolean, "1", 2, "3"]>>
 ];
 
 /* _____________ 다음 단계 _____________ */
