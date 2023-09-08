7496e4bf36901003e6eabb8175175fde2c0c2385
yusunghyun <ysh1513@naver.com>
2023-09-08 22:59:37 +0900

    ✅ 3057번 문제

--- playground/easy/03057-easy-push.ts
+++ playground/easy/03057-easy-push.ts
@@ -18,7 +18,7 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type Push<T, U> = any;
+type Push<T extends any[], U> = [...T, U];
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from "@type-challenges/utils";
@@ -26,7 +26,7 @@ import type { Equal, Expect } from "@type-challenges/utils";
 type cases = [
   Expect<Equal<Push<[], 1>, [1]>>,
   Expect<Equal<Push<[1, 2], "3">, [1, 2, "3"]>>,
-  Expect<Equal<Push<["1", 2, "3"], boolean>, ["1", 2, "3", boolean]>>,
+  Expect<Equal<Push<["1", 2, "3"], boolean>, ["1", 2, "3", boolean]>>
 ];
 
 /* _____________ 다음 단계 _____________ */
