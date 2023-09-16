6e50567ede5cfb779252ee559dfa03d0afe2c8c5
yusunghyun <ysh1513@naver.com>
2023-09-16 16:44:32 +0900

    ✅ 3번 문제

--- playground/medium/00003-medium-omit.ts
+++ playground/medium/00003-medium-omit.ts
@@ -28,14 +28,17 @@
 
 /* _____________ 여기에 코드 입력 _____________ */
 
-type MyOmit<T, K> = any;
+// 1. pick은 { [key in K]: T[key] }; 임. K값 들을 key로 넣으면 끝.
+// 2. omit은 pick과 반대됨. K가 "아닌" 것들을 key로 넣으면 끝.
+// 3. Exclude로 K의 여집합을 추출
+type MyOmit<T, K extends keyof T> = { [key in Exclude<keyof T, K>]: T[key] };
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from "@type-challenges/utils";
 
 type cases = [
   Expect<Equal<Expected1, MyOmit<Todo, "description">>>,
-  Expect<Equal<Expected2, MyOmit<Todo, "description" | "completed">>>,
+  Expect<Equal<Expected2, MyOmit<Todo, "description" | "completed">>>
 ];
 
 // @ts-expect-error
