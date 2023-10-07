e4f4026a49e3f738f6e66c7a6fbb3dca9dde4b07
yusunghyun <ysh1513@naver.com>
2023-10-08 00:28:52 +0900

    🔥 20번 문제 절반 통과

--- playground/medium/00020-medium-promise-all.ts
+++ playground/medium/00020-medium-promise-all.ts
@@ -20,10 +20,24 @@
 
   > GitHub에서 보기: https://tsch.js.org/20/ko
 */
-
 /* _____________ 여기에 코드 입력 _____________ */
 
-declare function PromiseAll(values: any): any;
+// 1. promise 벗기는 유틸 타입
+// 2. 재귀를 위해 타입 새로 선언
+// 3. shift와 유사하게 첫번째 추출하여 1. 사용 후 재귀 반복
+// 4. 재귀 마지막 한개 남았을 때 예외처리
+// 5. (A | B)[] 와 같은 타입 들어올 시 실패..ㅜ
+
+type PromiseInType<T> = T extends Promise<infer R> ? R : T;
+
+type Loop<T extends any[]> = T extends [infer R, ...infer E]
+  ? E["length"] extends 0
+    ? [PromiseInType<R>]
+    : [PromiseInType<R>, ...Loop<[...E]>]
+  : // 여기 왔다는건 (a|b)[]꼴.
+    never;
+
+declare function PromiseAll<T extends any[]>(values: T): Promise<Loop<T>>;
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from "@type-challenges/utils";
@@ -37,7 +51,7 @@ type cases = [
   Expect<Equal<typeof promiseAllTest1, Promise<[1, 2, 3]>>>,
   Expect<Equal<typeof promiseAllTest2, Promise<[1, 2, number]>>>,
   Expect<Equal<typeof promiseAllTest3, Promise<[number, number, number]>>>,
-  Expect<Equal<typeof promiseAllTest4, Promise<number[]>>>,
+  Expect<Equal<typeof promiseAllTest4, Promise<number[]>>>
 ];
 
 /* _____________ 다음 단계 _____________ */
